5c77ef61fbb6b5d47ae10a9467b2a4b73d158dd7
Nicolas Bohorquez <nicolas.bohorquez@alephsa.com>
2017-11-24 12:26:41 +0100

    Added v1 of reference project mind map

---
+++ Reference project/Graphs to INNOBAROMETER.mm
@@ -0,0 +1,244 @@
+<map version="1.0.1">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node CREATED="1511520469703" ID="ID_473457241" MODIFIED="1511520487351" TEXT="Graphs to INNOBAROMETER">
+<node CREATED="1511520501417" ID="ID_1827307328" MODIFIED="1511520503979" POSITION="right" TEXT="Authors">
+<node CREATED="1511520508737" ID="ID_110374713" MODIFIED="1511520512600" TEXT="Cinzia Carota"/>
+<node CREATED="1511520513081" ID="ID_180317842" MODIFIED="1511520514216" TEXT="Alessandra Durio"/>
+<node CREATED="1511520537966" ID="ID_680409404" MODIFIED="1511520537966" TEXT="Marco Guerzoni"/>
+</node>
+<node CREATED="1511520553178" ID="ID_35259105" MODIFIED="1511520567200" POSITION="left" TEXT="Objetives">
+<node CREATED="1511520613045" ID="ID_1157755809" MODIFIED="1511521847878">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Main
+    </p>
+    <p>
+      <font size="1">apply high-dimensional </font>
+    </p>
+    <p>
+      <font size="1">graphical models in </font>
+    </p>
+    <p>
+      <font size="1">innovation studies</font>
+    </p>
+  </body>
+</html>
+</richcontent>
+</node>
+</node>
+<node CREATED="1511520653468" ID="ID_1543153848" MODIFIED="1511520655815" POSITION="left" TEXT="CHIC">
+<node CREATED="1511520658717" ID="ID_1995761494" MODIFIED="1511521860990">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      C
+    </p>
+    <p>
+      <font size="1">The choice of the variables to include or to omit in </font>
+    </p>
+    <p>
+      <font size="1">the analysis is a crucial one, otherwise a perceived </font>
+    </p>
+    <p>
+      <font size="1">sloppiness in this step of the analysis would cast doubts of </font>
+    </p>
+    <p>
+      <font size="1">scientific malpractice such as the p-hacking behaviour.</font>
+    </p>
+  </body>
+</html>
+</richcontent>
+</node>
+<node CREATED="1511520684828" ID="ID_1244925409" MODIFIED="1511521871370">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      H
+    </p>
+    <p>
+      <font size="1">the use of graphical models as a tool to help </font>
+    </p>
+    <p>
+      <font size="1">the researcher in understanding the structure </font>
+    </p>
+    <p>
+      <font size="1">of a dataset as a whole, without omitting any variables </font>
+    </p>
+    <p>
+      <font size="1">on the base of educated guesses or introducing </font>
+    </p>
+    <p>
+      <font size="1">simplifying assumptions on existingrelationships</font>
+    </p>
+  </body>
+</html>
+</richcontent>
+</node>
+<node CREATED="1511520686874" ID="ID_1788916625" MODIFIED="1511520690205" TEXT="I">
+<node CREATED="1511521599898" ID="ID_53583852" MODIFIED="1511521606454" TEXT="Conceptual Design">
+<node CREATED="1511521626884" ID="ID_1572090466" MODIFIED="1511521829216">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      <font size="1">Classes of multivariate distributions whose conditional </font>
+    </p>
+    <p>
+      <font size="1">independence properties are encoded by a(n) (undirected) graph. </font>
+    </p>
+    <p>
+      
+    </p>
+    <p>
+      <font size="1">-&gt; conditional independence can be read in the graph</font>
+    </p>
+  </body>
+</html>
+</richcontent>
+</node>
+</node>
+<node CREATED="1511521607695" ID="ID_543243167" MODIFIED="1511521620101" TEXT="Research Design">
+<node CREATED="1511521889636" ID="ID_489751155" MODIFIED="1511521951214">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Dataset
+    </p>
+    <p>
+      <font size="1">Innobarameter is a dataset of 81 variables describing firms&#8217; </font>
+    </p>
+    <p>
+      <font size="1">innovative behaviour and collected via telephone-aided </font>
+    </p>
+    <p>
+      <font size="1">interviews to a stratified sample of 5234 European </font>
+    </p>
+    <p>
+      <font size="1">firms in the manufacturing sector.</font>
+    </p>
+  </body>
+</html>
+</richcontent>
+</node>
+<node CREATED="1511521955169" ID="ID_1440300162" MODIFIED="1511522072754">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Partition
+    </p>
+    <p>
+      <font size="1">Group the variables in five sets according </font>
+    </p>
+    <p>
+      <font size="1">to their economic meaning. Variables are </font>
+    </p>
+    <p>
+      <font size="1">grouped for the sake of clarity only and this </font>
+    </p>
+    <p>
+      <font size="1">partition does not play any role in the computation </font>
+    </p>
+    <p>
+      <font size="1">of the dependency structure.</font>
+    </p>
+  </body>
+</html>
+</richcontent>
+</node>
+<node CREATED="1511521989062" ID="ID_1336813293" MODIFIED="1511522151098">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Working dataset
+    </p>
+    <p>
+      <font size="1">52 binary variables and 9 variables taking a </font>
+    </p>
+    <p>
+      <font size="1">number of levels from 4 to 6 enter our analysis. </font>
+    </p>
+    <p>
+      <font size="1">They are listed in Table 1, third column. As </font>
+    </p>
+    <p>
+      <font size="1">mentioned in the previous section, the joint </font>
+    </p>
+    <p>
+      <font size="1">analysis of the structure of their association by </font>
+    </p>
+    <p>
+      <font size="1">high-dimensional graphical models implies a </font>
+    </p>
+    <p>
+      <font size="1">reduction of the sample size from 5234 to 1531</font>
+    </p>
+  </body>
+</html>
+</richcontent>
+</node>
+</node>
+</node>
+<node CREATED="1511520690541" ID="ID_573831985" MODIFIED="1511522415719">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      C
+    </p>
+    <p>
+      <font size="1">high-dimensional graphical models, </font>
+    </p>
+    <p>
+      <font size="1">and specifically simple minimum BIC </font>
+    </p>
+    <p>
+      <font size="1">forests, can serve as a data mining </font>
+    </p>
+    <p>
+      <font size="1">tool and guide the researcher towards </font>
+    </p>
+    <p>
+      <font size="1">a sound empirical design</font>
+    </p>
+  </body>
+</html>
+</richcontent>
+</node>
+</node>
+<node CREATED="1511522487827" ID="ID_624168275" MODIFIED="1511522490279" POSITION="right" TEXT="Tools">
+<node CREATED="1511522493916" ID="ID_1572684370" MODIFIED="1511522498599" TEXT="Analytical">
+<node CREATED="1511522512082" ID="ID_208445216" MODIFIED="1511522524772" TEXT="Graph theory"/>
+<node CREATED="1511522525494" ID="ID_1310719425" MODIFIED="1511522530931" TEXT="Network analysis"/>
+<node CREATED="1511522531575" ID="ID_33416967" MODIFIED="1511522564554" TEXT="Structure estimation"/>
+<node CREATED="1511522610018" ID="ID_1211785780" MODIFIED="1511522615148" TEXT="AIC / BIC forests"/>
+</node>
+<node CREATED="1511522499041" ID="ID_685015149" MODIFIED="1511522508364" TEXT="Practical">
+<node CREATED="1511522622456" ID="ID_1019935256" MODIFIED="1511522721886" TEXT="R"/>
+<node CREATED="1511522722803" ID="ID_417480346" MODIFIED="1511522723627" TEXT="gRapHD"/>
+</node>
+</node>
+</node>
+</map>
